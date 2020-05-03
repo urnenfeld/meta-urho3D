751ff631b658692fdfce83dec3f09e1dd1b93c02
urnenfeld <urn@castell>
2020-05-03 13:14:59 +0200

    ensure flushing I/O

--- files/flashSD.sh
+++ files/flashSD.sh
@@ -6,3 +6,4 @@ echo Ensure invoking from build directory, review and press key or ^C ...
 read something
 
 sudo dd if=./tmp/deploy/images/$MACHINE/$IMAGE-$MACHINE.rpi-sdimg bs=1M of=$1 status=progress
+sync
