6b9d72450681e7f92b6721326bbc60a08e6bfa46
urnenfeld <urn@castell>
2020-05-30 13:03:43 +0200

    Split images recipes

--- files/flashSD.sh
+++ files/flashSD.sh
@@ -1,5 +1,6 @@
 MACHINE="raspberrypi0-wifi"
-IMAGE="core-image-urho3d"
+# IMAGE="core-image-urho3d"
+IMAGE="urhobox"
 
 echo sudo dd if=./tmp/deploy/images/$MACHINE/$IMAGE-$MACHINE.rpi-sdimg of=$1
 echo Ensure invoking from build directory, review and press key or ^C ...
